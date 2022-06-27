4e785f102f06b6fb27e145d79df662c7138ba7e0
Eric Liu <liuer@eeee.wifi.local.cmu.edu>
2022-06-27 11:32:08 -0400

    Complete preliminary MembershipOracle

--- lts-helper/src/main/kotlin/edu/cmu/isr/lts/MembershipOracle.kt
+++ lts-helper/src/main/kotlin/edu/cmu/isr/lts/MembershipOracle.kt
@@ -4,17 +4,34 @@ import de.learnlib.filter.cache.dfa.DFAHashCacheOracle
 import de.learnlib.api.oracle.MembershipOracle
 import de.learnlib.api.query.Query
 import net.automatalib.automata.fsa.impl.compact.CompactDFA
+import net.automatalib.words.Word
 
-class WeakestMembershipOracle<I, D>(machine: CompactDFA<I>, property: CompactDFA<I>) : MembershipOracle<I, D> {
-    val machineLTS: CompactDetLTS<I> = machine.asLTS()
-    val propertyLTS: CompactDetLTS<I> = property.asLTS()
+class WeakestMembershipOracle<I, D>(machine: CompactDFA<I>, property: CompactDFA<I>) : MembershipOracle.DFAMembershipOracle<I> {
+    private val machineLTS: CompactDetLTS<I> = machine.asLTS()
+    private val propertyLTS: CompactDetLTS<I> = property.asLTS()
+    val composition = parallelComposition(machineLTS, machineLTS.inputAlphabet, propertyLTS, propertyLTS.inputAlphabet)
 
-    override fun processQueries(p0: MutableCollection<out Query<I, D>>?) {
-        // @TODO
+    override fun processQueries(p0: MutableCollection<out Query<I, Boolean>>?) {
+        if (p0 != null) {
+            for(query: Query<I, Boolean> in p0) {
+                processQuery(query)
+            }
+        }
     }
-
-    override fun processQuery(query: Query<I, D>?) {
+    override fun processQuery(query: Query<I, Boolean>?) {
         // @TODO
+        // run string in query over composition, resolve false if ends in error state
+        val input: Word<I>? = query?.input
+
+        //null check
+        if(query != null) {
+            if (composition.accepts(input)) {
+                query.answer(true)
+            } else {
+                query.answer(false)
+            }
+        }
     }
 
 }
+
